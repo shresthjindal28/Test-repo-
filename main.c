da6b72a5c219a044a10ff959d438bf239e9b20d1
ROHIT DUBEY <rohitdubey713010@gmail.com>
2025-08-21 11:02:44 +0530

    Added some new code

--- main.c
+++ main.c
@@ -4,5 +4,10 @@ int main(){
     int a=2,b=3;
     printf("Hello World");
     printf("a+b is ",a+b);
+    printf("a+b is ",a+b);
+    printf("a+b is ",a+b);
+    printf("a+b is ",a+b);
+    printf("a+b is ",a+b);
+
     return 0;
 }
