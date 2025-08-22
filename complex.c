df65e4137523ff9c1951ccafb556e806d75accd7
shadow <shadowcipher01@gmail.com>
2025-08-22 23:54:59 +0530

    Create complex.c

---
+++ complex.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int mystery(int *arr, int n) {
+    if (n == 0) return 0;
+    return (arr[n - 1] ^ n) + mystery(arr, n - 1);
+}
+
+int main() {
+    int data[] = {5, 7, 12, 3};
+    int size = sizeof(data) / sizeof(data[0]);
+    printf("Result: %d\n", mystery(data, size));
+    return 0;
+}
