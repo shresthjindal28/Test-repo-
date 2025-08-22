dcdb9f4b2c3d455d25528e33842eb7d805e2bcac
shadow <shadowcipher01@gmail.com>
2025-08-22 11:36:21 +0530

    Update sum.c

--- sum.c
+++ sum.c
@@ -3,9 +3,11 @@
 int main(){
   // This adds, multiplies, and subtracts 2 numbers
   int a = 5, b = 10;
-
+  
   printf("sum = %d\n", a + b);
   printf("product = %d\n", a * b);
+
+  // this prints diff
   printf("difference = %d\n", a - b);
 
   return 0;
