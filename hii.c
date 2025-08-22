e45c8b71e56c6adbde92aa940e114e414de06788
ROHIT DUBEY <rohitdubey713010@gmail.com>
2025-08-22 12:39:29 +0530

    changes

--- hii.c
+++ hii.c
@@ -1,6 +1,9 @@
-int main(int argc, char const *argv[])
-{
-    /* code */
-    
+#include <stdio.h>
+
+int main() {
+    int arr[3] = {1, 2, 3};
+
+    printf("High: %d\n", arr[5]);  
+
     return 0;
 }
