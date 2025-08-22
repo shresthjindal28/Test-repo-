3fb58c3b6728d0d972b8c196077e564acd67aa20
ROHIT DUBEY <rohitdubey713010@gmail.com>
2025-08-22 12:41:04 +0530

    new code

--- hello.c
+++ hello.c
@@ -3,6 +3,11 @@ int main()
 {
   int a,b;
   a=b=10;
+  a=b=10;
+  a=b=10;
+  a=b=10;
+  a=b=10;
+  a=b=10;
   /* printf("Hello Wrold from faris & Shresth %d",a+b); */
   return 0;
 }
--- hii.c
+++ hii.c
@@ -4,6 +4,8 @@ int main() {
     int arr[3] = {1, 2, 3};
 
     printf("High: %d\n", arr[5]);  
+    printf("High: %d\n", arr[5]);  
+    printf("High: %d\n", arr[5]);  
 
     return 0;
 }
