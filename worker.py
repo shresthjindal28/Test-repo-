9cdfbb5016b679fa2ede36138afd7dceb453b07c
shadow <shadowcipher01@gmail.com>
2025-08-23 17:02:43 +0530

    Create worker.py

---
+++ worker.py
@@ -0,0 +1,13 @@
+import redis, json, pprint, time
+
+r = redis.Redis()
+queue_name = "alphaQ"
+
+while True:
+    raw_task = r.brpop(queue_name, timeout=5)  # waits max 5 seconds
+    if raw_task:
+        _, task_json = raw_task
+        task = json.loads(task_json)
+        pprint.pprint(task)
+    else:
+        print("No task arrived, waiting again...")
