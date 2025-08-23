06e032173462359fdf4987a2a398867153b5137b
shadow <shadowcipher01@gmail.com>
2025-08-23 10:26:32 +0530

    Update hello.c

--- hello.c
+++ hello.c
@@ -5,7 +5,6 @@ int main()
   a=b=10;
   a=b=10;
   a=b/00000;
- // this is it. We won it
   a=b=10;
    printf("Hello Wrold from faris & Shresth %d",a+b); 
   printf("Hello Wrold from faris & Shresth %d",a+b); 
