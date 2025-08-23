2407df52455384f045c5206ffee3b1726b7ac65a
shadow <shadowcipher01@gmail.com>
2025-08-23 10:29:06 +0530

    Update hello.c

--- hello.c
+++ hello.c
@@ -6,6 +6,7 @@ int main()
   a=b=10;
   a=b/00000;
   a=b=10;
+  // THis will rpint
    printf("Hello Wrold from faris & Shresth %d",a+b); 
   printf("Hello Wrold from faris & Shresth %d",a+b); 
   return 0;
